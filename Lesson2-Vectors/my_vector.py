1e7e97942e7f11bb2ccad35303fc246de30c5e59
ziheng <zihenglv@gmail.com>
2018-02-27 09:51:32 +0800

    modify: add comments for my_vector.py

--- Lesson2-Vectors/my_vector.py
+++ Lesson2-Vectors/my_vector.py
@@ -5,36 +5,46 @@ class Vector(object):
             self.coordinates = tuple(coordinates)
             self.dimension   = len(coordinates)
         except:
-            print("Error in init")
+            print("Error format")
+
     def __str__(self):
         return "Vector: {}".format(self.coordinates)
     def __eq__(self, vector):
         return self.coordinates == vector.coordinates
 
+    # plus: [1, 2] + [3, 4] = [4, 6]
     def plus(self, vector):
-        new_coordinates = []
+        new_coordinates = list()
         for i in range(len(self.coordinates)):
             new_coordinates.append(self.coordinates[i] + vector.coordinates[i])
         return Vector(new_coordinates)
+
+    # minus: [4, 3] - [1, 2] = [3, 1]
     def minus(self, vector):
         new_coordinates = []
         for i in range(len(self.coordinates)):
             new_coordinates.append(self.coordinates[i] - vector.coordinates[i])
         return Vector(new_coordinates)
+
+    # scalar_mul: [3, 4] * 2  = [6, 8]
     def scalar_mul(self, c):
         new_coordinates = []
         for i in range(len(self.coordinates)):
             new_coordinates.append(self.coordinates[i] * c)
         return Vector(new_coordinates)
+
+    # magnitude: [3, 4] => sqrt( 3^2 + 4^2 ) = 5
     def magnitude(self):
         coordinates_squared = []
         for i in range(len(self.coordinates)):
             coordinates_squared.append(self.coordinates[i] ** 2)
         return math.sqrt(sum(coordinates_squared))
+
+    # normalized: [3, 4] => 1/5 * [3, 4] = [3/5, 4/5]
     def normalized(self):
         try:
             magnitudes = self.magnitude()
             return self.scalar_mul(1/magnitudes)
         except:
-            print("Error in normalized")
+            print("Error when normalized")
 
